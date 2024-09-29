40509a5f220fa380a1e74322a8082203934ec1d0
Dasha Jam <dashajambox@gmail.com>
2024-09-29 16:34:19 -0700

    сделаный 5 спринт

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,19 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector("#card-template").content;
+const cardList = document.querySelector(".places__list");
+
+initialCards.forEach(function (element) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__title").textContent = element.name;
+  cardElement.querySelector(".card__image").setAttribute("src", `${element.link}`);
+
+  cardElement.querySelector(".card__delete-button").addEventListener("click", function (evt) {
+      evt.target.closest(".card").remove();
+    });
+
+  cardList.append(cardElement);
+});
