6047d16f46496e73bfde3cf7ed6629389c1bd3a4
Dasha Jam <dashajambox@gmail.com>
2026-05-21 12:06:52 +0300

    добавлено создание, удаление карточек из массива данных

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,4 +1,4 @@
-const INITIAL_CARDS = [
+const initialCards = [
     {
         name: 'Архыз',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
@@ -24,5 +24,3 @@ const INITIAL_CARDS = [
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
     },
 ];
-
-export { INITIAL_CARDS };
---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+const cardTemplate = document.querySelector('#card-template').content;
+
+const cardList = document.querySelector('.places__list');
+
+const createCard = (cardData, deleteCard) => {
+
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    const cardTitle = cardElement.querySelector('.card__title');
+    const cardImage = cardElement.querySelector('.card__image');
+
+    cardTitle.textContent = cardData.name;
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+
+    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+
+    cardDeleteButton.addEventListener('click', deleteCard);
+
+    return cardElement;
+
+};
+
+const deleteCard = (event) => {
+    event.target.closest('.card').remove();
+};
+
+initialCards.forEach((card) => {
+    cardList.append(createCard(card, deleteCard));
+});
