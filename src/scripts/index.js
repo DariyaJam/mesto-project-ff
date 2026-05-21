5cfef892bef3f8105f9d550191e5618b8e34c408
Dasha Jam <dashajambox@gmail.com>
2026-05-21 18:48:25 +0300

    Добавлена возможность поменять данные профиля

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,35 +9,55 @@ const cardList = document.querySelector('.places__list');
 
 const profileEditButton = document.querySelector('.profile__edit-button');
 const profileEditPopup = document.querySelector('.popup_type_edit');
+const profileEditForm = document.forms['edit-profile'];
+
+const userData = document.querySelector('.profile__info');
+const userName = userData.querySelector('.profile__title');
+const userDescription = userData.querySelector('.profile__description');
 
 const createCardButton = document.querySelector('.profile__add-button');
 const createCardPopup = document.querySelector('.popup_type_new-card');
+const createCardForm = document.forms['new-place'];
 
 const openCardImagePopup = document.querySelector('.popup_type_image');
 
 const closePopupButtons = document.querySelectorAll('.popup__close');
+
+const editAvatarForm = document.forms['edit-avatar'];
 
 initialCards.forEach((card) => {
     cardList.append(createCard(card, deleteCard));
 });
 
 const handleProfileEditPopupOpen = () => {
+    profileEditForm.name.value = userName.textContent;
+    profileEditForm.description.value = userDescription.textContent;
+
     openModal(profileEditPopup);
     profileEditPopup.addEventListener('click', closeModalByOverlay);
+};
+
+function handleProfileEditFormSubmit(event) {
+    event.preventDefault();
+
+    userName.textContent = profileEditForm.name.value;
+    userDescription.textContent = profileEditForm.description.value;
 }
+
+profileEditForm.addEventListener('submit', handleProfileEditFormSubmit);
 
 const handleCreateCardPopupOpen = () => {
     openModal(createCardPopup);
     createCardPopup.addEventListener('click', closeModalByOverlay);
-}
+};
 
 const handleImagePopupOpen = () => {
 
-}
+};
 
 const handleClosePopup = () => {
     closeModal(document.querySelector('.popup_is-opened'));
-}
+};
 
 closePopupButtons.forEach(button => {
     button.addEventListener('click', handleClosePopup);
