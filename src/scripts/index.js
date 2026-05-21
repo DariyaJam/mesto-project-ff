89df76022269c402455248fdc917f687aa28ceca
Dasha Jam <dashajambox@gmail.com>
2026-05-21 18:17:58 +0300

    Добавлено закрытие модальных окон с помощью кнопки, overlay и escape

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,16 +1,21 @@
 import '@/pages/index.css';
 import { initialCards } from "@/scripts/cards";
-import { openModal } from "@/scripts/modal";
+import { openModal, closeModal, closeModalByOverlay } from "@/scripts/modal";
 import { createCard, deleteCard } from "@/scripts/card";
 
 export const cardTemplate = document.querySelector('#card-template').content;
 
 const cardList = document.querySelector('.places__list');
-const profileEditButton = document.querySelector('.profile__edit-button');
-const addCardButton = document.querySelector('.profile__add-button');
 
+const profileEditButton = document.querySelector('.profile__edit-button');
 const profileEditPopup = document.querySelector('.popup_type_edit');
+
+const createCardButton = document.querySelector('.profile__add-button');
 const createCardPopup = document.querySelector('.popup_type_new-card');
+
+const openCardImagePopup = document.querySelector('.popup_type_image');
+
+const closePopupButtons = document.querySelectorAll('.popup__close');
 
 initialCards.forEach((card) => {
     cardList.append(createCard(card, deleteCard));
@@ -18,12 +23,26 @@ initialCards.forEach((card) => {
 
 const handleProfileEditPopupOpen = () => {
     openModal(profileEditPopup);
+    profileEditPopup.addEventListener('click', closeModalByOverlay);
 }
 
 const handleCreateCardPopupOpen = () => {
     openModal(createCardPopup);
+    createCardPopup.addEventListener('click', closeModalByOverlay);
 }
 
+const handleImagePopupOpen = () => {
+
+}
+
+const handleClosePopup = () => {
+    closeModal(document.querySelector('.popup_is-opened'));
+}
+
+closePopupButtons.forEach(button => {
+    button.addEventListener('click', handleClosePopup);
+});
+
 profileEditButton.addEventListener('click', handleProfileEditPopupOpen);
-addCardButton.addEventListener('click', handleCreateCardPopupOpen);
+createCardButton.addEventListener('click', handleCreateCardPopupOpen);
 
--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,6 +1,25 @@
+const closeModalByKey = (event) => {
+    if (event.key === 'Escape') {
+        closeModal(document.querySelector('.popup_is-opened'));
+    }
+};
+
+const closeModalByOverlay = (event) => {
+    if (event.target.classList.contains("popup_is-opened")) {
+        closeModal(document.querySelector(".popup_is-opened"));
+    }
+}
+
 const openModal = (popup) => {
     popup.classList.add('popup_is-animated');
     popup.classList.add('popup_is-opened');
+
+    document.addEventListener('keydown', closeModalByKey);
 };
 
-export { openModal };
+const closeModal = (popup) => {
+    popup.classList.remove('popup_is-opened');
+    popup.classList.remove('popup_is-animated');
+};
+
+export {openModal, closeModal, closeModalByOverlay};
